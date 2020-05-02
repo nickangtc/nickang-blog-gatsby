c198b130c8105369175b8d46bc790e06fa4c30aa
Nick Ang <nickangtc@gmail.com>
2020-05-02 18:30:55 +0200

    Add scoped style with css module

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import styles from '../styles/about.module.scss'
+
 const AboutPage = () => {
     return (
         <div>
             <h1>About</h1>
             <p>Hi, I'm Nick.</p>
-            <p><Link to="/contact">Contact info.</Link></p>
+            <p><Link className={styles.link} to="/contact">Contact info.</Link></p>
         </div>
     )
 }
