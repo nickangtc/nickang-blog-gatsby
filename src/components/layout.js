4e6ef864c957dda3e3e9aae85688c843d6ea23c2
Nick Ang <nickangtc@gmail.com>
2020-05-17 17:32:37 +0200

    Abstract Header component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,67 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
 
+import Header from '../components/header'
 import Footer from '../components/footer'
 import '../styles/global.scss'
 import layoutStyles from './layout.module.scss'
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `"Inter var",-apple-system,BlinkMacSystemFont,"Helvetica Neue",Helvetica,sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-  }
+  // const rootPath = `${__PATH_PREFIX__}/`
   return (
     <div
       style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
         maxWidth: rhythm(24),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
       className={layoutStyles.container}
     >
       <div className={layoutStyles.content}>
-        <header>{header}</header>
+        <Header title={title}></Header>
         <main>{children}</main>
       </div>
       <Footer></Footer>
