bc7ce5d4e40643702b19bc4fe17e2b130623af45
Nick Ang <nickangtc@gmail.com>
2020-05-17 17:47:05 +0200

    Add navbar; Remove bio from all posts page

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from "gatsby"
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
+import headerStyles from "./header.module.scss"
 
 const Header = ({ title }) => {
   return (
     <header>
       <h1
         style={{
-          ...scale(1.2),
           marginBottom: rhythm(1.5),
           marginTop: 0
         }}
@@ -23,6 +23,23 @@ const Header = ({ title }) => {
           {title}
         </Link>
       </h1>
+      
+      <nav>
+          <ul className={ headerStyles.navList }>
+            <li className={ headerStyles.navItem }>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li className={ headerStyles.navItem }>
+              <Link to="/newsletter">Newsletter</Link>
+            </li>
+            <li className={ headerStyles.navItem }>
+              <Link to="/recommendations">Recommendations</Link>
+            </li>
+            <li className={ headerStyles.navItem }>
+              <Link to="/now">Now</Link>
+            </li>
+          </ul>
+      </nav>
     </header>
   )
 }
