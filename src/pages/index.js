1b6072efe141d7eb1bc2f1bb447d605f90732a05
Nick Ang <nickangtc@gmail.com>
2020-05-17 16:30:02 +0200

    Add Now page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,28 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-const BlogIndex = ({ data, location }) => {
+const HomePage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="Nick Ang's digital garden" />
+      <SEO title="Nick Ang" />
       Front page content to come.
     </Layout>
   )
 }
 
-export default BlogIndex
+export default HomePage
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD.MM.YYYY")
-            title
-            description
-          }
-        }
       }
     }
   }
