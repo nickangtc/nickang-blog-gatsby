30f79fbd024cb6356b27b35984f812510e215f6a
Nick Ang <nickang@Nick-MBP.local>
2021-10-24 00:27:28 +0200

    Update front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,10 @@ const HomePage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SearchEngineOptimisation title="Home" location={location} />
       <h1 id="my-digital-garden">Hey, I'm Nick. <span role="img" aria-label="hand wave emoji">👋</span></h1>
-      <p>I'm an Engineering Team Lead at <a href="https://smartly.io">Smartly.io</a>. I write about a broad range of things that I learn and observe, although they seem to coalesce around topics like tech, living a meaningful life, and communicating well.</p>
-      <p>New articles published every Sunday. <Link to="/subscribe">Subscribe</Link> for free occasional updates.</p>
+      <p>I'm currently an Engineering Team Lead at <a href="https://smartly.io">Smartly.io</a>. I write about a broad range of things that I learn and observe, and they seem to coalesce around topics like technology, living a meaningful life, and communicating well.</p>
+      <p>I publish new articles every Sunday. <Link to="/subscribe">Subscribe</Link> for free occasional updates. I try to be helpful with each article I publish. If something you read helped you, please <Link to="/contact">reach out</Link> and/or <a target="_blank" rel="noreferrer" href={`https://ko-fi.com/nickang`}>buy me a coffee</a>.</p>
       <p>If you're new here and not sure where to start, I recommend reading something recent below or <Link to="/topics">browsing by topic</Link>.</p>
-
+      
       <h3>Recent articles</h3>
       <ul className={ articles }>
         { newestPosts }
