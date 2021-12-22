e755dec9707b20f6b8c9ee4b1cddff9c2ae0f062
Nick Ang <nickangtc@gmail.com>
2021-12-22 18:36:17 +0100

    Add Everydays to navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,47 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image"
 
 import { siteTitle, navList, navItem, active } from "./header.module.scss"
 
 const Header = ({ title }) => {
-  const data = useStaticQuery(graphql`query HeaderQuery {
-  site {
-    siteMetadata {
-      author {
-        name
-        summary
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+        }
       }
     }
-  }
-}
-`)
+  `)
 
   return (
-    <header style={{ display: 'flex' }}>
+    <header style={{ display: "flex" }}>
       <StaticImage
         src="../../content/assets/nick-ang-profile-photo-square-jun-2018-min.jpg/"
         imgStyle={{
           borderRadius: `50%`,
-        }} 
-        alt={ `${data.site.siteMetadata.author.name} profile picture` }
+        }}
+        alt={`${data.site.siteMetadata.author.name} profile picture`}
         style={{
           marginBottom: 0,
-          marginRight: '1rem',
+          marginRight: "1rem",
           maxWidth: 75,
           maxHeight: 75,
           borderRadius: `100%`,
-        }} />
-      <section style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}>
-        <h1 className={ siteTitle }>
+        }}
+      />
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1 className={siteTitle}>
           <Link
             style={{
               boxShadow: `none`,
@@ -50,15 +54,26 @@ const Header = ({ title }) => {
         </h1>
 
         <nav>
-          <ul className={ navList }>
-            <li className={ navItem }>
-              <Link to="/blog" activeClassName={ active }>Blog</Link>
+          <ul className={navList}>
+            <li className={navItem}>
+              <Link to="/blog" activeClassName={active}>
+                Blog
+              </Link>
             </li>
-            <li className={ navItem }>
-              <Link to="/topics" activeClassName={ active }>Topics</Link>
+            <li className={navItem}>
+              <Link to="/everydays" activeClassName={active}>
+                Everydays
+              </Link>
             </li>
-            <li className={ navItem }>
-              <Link to="/subscribe" activeClassName={ active }>Subscribe</Link>
+            <li className={navItem}>
+              <Link to="/topics" activeClassName={active}>
+                Topics
+              </Link>
+            </li>
+            <li className={navItem}>
+              <Link to="/subscribe" activeClassName={active}>
+                Subscribe
+              </Link>
             </li>
           </ul>
         </nav>
