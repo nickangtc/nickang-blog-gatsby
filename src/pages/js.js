497ecb9c8b791a82140de2f8d10dd2c7cb7e325a
Nick Ang <nickangtc@gmail.com>
2023-05-07 22:59:26 +0200

    Update job search page w testimonials

--- src/pages/js.js
+++ src/pages/js.js
@@ -10,7 +10,7 @@ const JobSearchPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SearchEngineOptimisation title="Nick's Job Search" location={location} />
-      <h1>Details for recruiters</h1>
+      <h1>My next job wishlist</h1>
       <p>
         Hi! Thank you for visiting this obscure page on the internet. I was laid
         off by Shopify as part of a mass layoff in 2023 where 20% of the global
@@ -23,16 +23,36 @@ const JobSearchPage = ({ data, location }) => {
         </a>{" "}
         is my most up-to-date resume.
       </p>
-      <p>
-        While I figure out what exactly I want (that's honestly such a tough
-        thing to do!), I'm sharing a list of hard requirements that I know I
-        don't want 🚫 to be working on next, which is meant to save both you and
-        me some time:
-      </p>
-      <ul>
-        <li>No fully in-person work (prefer remote, or hybrid)</li>
-        <li>No roles based outside of Germany</li>
-      </ul>
+      <p>More details to come.</p>
+      <p>What people have said about me:</p>
+      <blockquote>
+        "Nick is technically strong, a fast learner and takes initiative to
+        solve problems. He is a team player and a great communicator and will be
+        a great value to any team that he is a part of." - Justin Y.
+      </blockquote>
+      <blockquote>
+        "I've personally seen him rock it as a writer, as a team lead, as a
+        software engineer, as a project manager, as a mentor, and as a brilliant
+        coworker. Working with Nick really made me feel like I belonged in the
+        team." - Milan R.
+      </blockquote>
+      <blockquote>
+        "I had the privilege of being taught by Nick during my time at General
+        Assembly. Nick was one of my teaching assistants in the course and he
+        was one of the better educators I ever had in my learning journey thus
+        far. Nick is patient, logical, and approaches every single challenge
+        with a zealous attitude. I am really honoured to have been taught by
+        such a marvellous educator." - Zhao L.
+      </blockquote>
+      <blockquote>
+        "I thoroughly enjoyed having Nicholas as a teacher. He is very
+        knowledgeable yet able to explain difficult concepts clearly. He made
+        the course exciting and interactive, keeping us all engaged. Nicholas
+        was always approachable and did go out of his way to help the class
+        understand concepts, making himself available outside office hours to
+        solve queries, provide guidance debugging code and even keep our spirits
+        up!" - Laura G.
+      </blockquote>
     </Layout>
   )
 }
