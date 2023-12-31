94d4c2724c15ee99b966ffb853369d28e6475167
Nick Ang <nickangtc@gmail.com>
2023-12-31 23:06:34 +0100

    Update books page content

--- src/pages/books.js
+++ src/pages/books.js
@@ -29,16 +29,23 @@ const BooksPage = ({ data, location }) => {
       <SearchEngineOptimisation title="Books list" location={location} />
       <h1>Books list</h1>
       <p>
-        In 2024 I finally started a spreadsheet to track the books I read. My
-        goal is simply to read more, and part of the strategy is to have a
-        consistent supply of interesting books written down somewhere.{" "}
+        Just like you, I'm trying to read more, period. I currently read an
+        average of 10 books a year (including the ones I choose to give up on),
+        which I think is a measly portion of what I think I ought to consume.
+        Currenty, I'm inclined to read broadly; this means I'll read a mix of
+        genres.
+      </p>
+      <p>
+        I've made my longlist of books available to the public. In case you're
+        interested,{" "}
         <a
           href="https://docs.google.com/spreadsheets/d/10BBp2cYszi8LZxOmcOKyZIg2edmZrpGxDyG56XJrmS0/edit?usp=sharing"
           target="_blank"
         >
-          Open the spreadsheet
+          open the spreadsheet
         </a>{" "}
-        to see what's on my list right now.
+        to see what I'm reading, what I've read, and what I'm planning to read.
+        Below are the notes I've written for books I've read.
       </p>
       <ul>{booksPosts}</ul>
     </Layout>
