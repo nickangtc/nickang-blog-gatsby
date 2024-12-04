1e412641aabdf66f44f62ad048be4db7cbea0615
Nick Ang <nickangtc@gmail.com>
2024-12-04 08:11:25 +0100

    road trip

--- new.js
+++ new.js
@@ -68,7 +68,7 @@ title: "${postTitle}"
 date_published: "${date}"
 date_updated: "${date}"
 excerpt:
-tags: ["Tech", "Money", "Interviewing", "Career", "Living", "Creativity", "PKM", "Leadership", "Communication", "Productivity", "Fiction", "Good intentions", "Book", "Tutorial", "Collection", "Announcement"]
+tags: ["Tech", "Money", "Interviewing", "Career", "Living", "Creativity", "PKM", "Leadership", "Communication", "Productivity", "Fiction", "Good intentions", "Book", "Tutorial", "Collection", "Announcement", "Parenting"]
 fav: false
 creation_duration_minutes:
 ---
